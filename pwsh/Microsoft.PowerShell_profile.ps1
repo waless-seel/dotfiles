96efbd179ec11405b48dc30a06dbd5a2ab7eaf34
Test User <seless.yuu@outlook.jp>
2026-05-03 20:27:48 +0900

    mise管理pythonを安定板に変更 mise管理にuvを追加 インストールバージョンのscriptsフォルダにパスが通るよう対応

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -6,5 +6,11 @@ Set-PSReadLineOption -PredictionSource History -ErrorAction SilentlyContinue
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+$_pythonHome = & mise where python 2>$null
+if ($LASTEXITCODE -eq 0 -and $_pythonHome) {
+    $env:PATH = "$_pythonHome\Scripts;$env:PATH"
+}
+Remove-Variable _pythonHome -ErrorAction SilentlyContinue
+
 . "$PSScriptRoot\pwsh_git.ps1"
 . "$PSScriptRoot\pwsh_util.ps1"
